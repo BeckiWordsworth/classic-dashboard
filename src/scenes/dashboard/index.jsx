4b6f0c5f31b7741437ea4a5ac1cba49dd4a28e71
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-01-23 22:35:52 +0100

    Teams page

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "../../components/common/Header";
+import { Box, Typography, useTheme } from "@mui/material";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box m="20px">
+      <Box display="flex" justifyContent={"space-between"} alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your data" />
+      </Box>
+    </Box>
+  );
 };
 
 export default Dashboard;
