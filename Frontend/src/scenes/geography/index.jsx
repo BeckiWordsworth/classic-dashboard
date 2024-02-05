dd89ccdfdcb4f45a64278ed61e82fb22f6eb7fb1
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-02-05 22:40:42 +0100

    Perfecting the geography charts

--- Frontend/src/scenes/geography/index.jsx
+++ Frontend/src/scenes/geography/index.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Box } from "@mui/material";
 import Header from "../../components/common/Header";
+import { tokens } from "../../theme";
+import { useTheme } from "@mui/material";
 import GeopgraphyChart from "../../components/common/GeographyChart";
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
     <Box m="20px">
       <Header title="Geography Chart" subtitle="Simple Geography Chart" />
-      <Box height="75vh">
+      <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius="4px">
         <GeopgraphyChart />
       </Box>
     </Box>
